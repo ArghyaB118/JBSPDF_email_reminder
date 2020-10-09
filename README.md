4545ae354a4b29d5490827eafe13b44ecc06964f
Arghya Bhattacharya <iamarghya.1@gmail.com>
2020-10-09 15:26:48 -0400

    new automations addded

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 # JBSPDF_email_reminder
-This is a repository where I am uploading the code to automate sending email reminders for JBSPDF members. The code accesses a csv to know people assigned to me (one of the admins) and before 7 days it sends an automated reminder to post the thought of the day for the Slack forum of JBSPDF (jagadish Bose Scholars Professional Development Forum). The code uses SMTP protocol. Hence in order to use it, please switch to 'less secure app access' from google account settings.
+This is a repository where I am uploading the code to automate sending email reminders for JBSPDF members. The code accesses a .csv file to know people assigned to me (one of the admins) and within 7 days from today it sends an automated reminder to post the #thought_of_the_day for the Slack forum of JBSPDF (Jagadish Bose Scholars Professional Development Forum). 
 
-##How to run?
+## Protocol we use
+The code uses SMTP protocol. Hence in order to use it, please switch to 'less secure app access' from google account settings.
+
+## Data format in .csv
+```text
+day_assigned,mail_id,name,admin,mail_sent
+2020-10-10,confidentrana118@gmail.com,Arghya Bhattacharya,Arghya,FALSE
+```
+
+##Data Format in credentials
+```text
+Mail ID
+Mail Password
+First Name
+```
+
+## How to run?
 ```bash
 pip install pandas DateTime smtplib ssl
 python send_email.py
--- send_mail.py
+++ send_mail.py
@@ -1,42 +1,35 @@
-sender_email = "your mail ID"
-password = "your password for mail"
 #switch on the 'less secure app access from google account settings'
-#=TEXTJOIN(",",TRUE,K2,B2,J2)
+#=TEXTJOIN(",",TRUE,K2,B2,J2,M2)
 #maintain the datetime.date format the same as excel or vice versa
 import smtplib, ssl
 import datetime
 import pandas as pd
-today = datetime.date.today() + datetime.timedelta(days=7)
-print(today)
+today = datetime.date.today()
+print "Operation done on", str(today)
+
+#getting the target dates for sending reminder
+#dates may lie anywhere within a week from now
+target_dates = []
+for i in range(1,8):
+	target_dates.append(datetime.date.today() + datetime.timedelta(days=i))
+
+#read the data file
 data = pd.read_csv("filename.csv")
-print(data.size)
 
-for ind in data.index:
-	if data['day_assigned'][ind] == str(today):
-		if data['admin'][ind] == "Arghya":
-			receiver_name = str(data['name'][ind])
-			receiver_email = str(data['mail_id'][ind])
-			receiver_day = str(data['day_assigned'][ind])
-			print(data['mail_id'][ind], data['day_assigned'][ind])
+#read the credentials from a text file
+with open('credentials.txt', "r") as cred:
+    credentials = [line.rstrip() for line in cred]
 
+sender_email = credentials[0]
+password = credentials[1]
+admin = credentials[2]
+
+print sender_email, password, admin
+cred.close()
+
+#setting up the SMTP server 
 smtp_server = "smtp.gmail.com"
 port = 587  # For starttls
-
-message = """\
-Subject: Invitation to post #thought_of_the_day on JBSPDF Slack forum
-
-Hi %s!
-
-You probably have received an email from jbspdf about the initiative that has been taken to encourage members, especially younger scholars like you to post the #thought_of_the_day. As was mentioned in the email, it can be any "science communication" that you find interesting. This can include latest science news, important scientific concepts, some scientific proof, and more than that. We would like to invite you to post your #thought_of_the_day on %s.
-
-Best,
-Arghya.""" % (receiver_name, receiver_day)
-
-
-#message = """\ 
-#Subject: Invitation to post #thought_of_the_day on JBSPDF Slack forum
-
-#Hi %s, This is just a gentle reminder to post the thought of the day on %s""" % (receiver_name, receiver_day)
 
 # Create a secure SSL context
 context = ssl.create_default_context()
@@ -48,10 +41,33 @@ try:
 	server.starttls() # Secure the connection
 	server.ehlo() # Can be omitted
 	server.login(sender_email, password)
-	server.sendmail(sender_email, receiver_email, message)
-	# TODO: Send email here
+
+	for ind in data.index:
+		for target_date in target_dates:
+			if data['day_assigned'][ind] == str(target_date):
+				if data['admin'][ind] == admin:
+					if data['mail_sent'][ind] == 0:
+						receiver_name = str(data['name'][ind])
+						receiver_email = str(data['mail_id'][ind])
+						receiver_day = str(data['day_assigned'][ind])
+						print "Mailing to: ", receiver_email, "for date:", receiver_day
+						confirm = raw_input("Please confirm by saying 'yes' or 'no: ")
+						if confirm == "yes":
+							#compose the text
+							message = """\
+Subject: Invitation to post #thought_of_the_day on JBSPDF Slack forum
+
+Hi %s!
+
+You probably have received an email from JBSPDF about the initiative that has been taken to encourage members, especially younger scholars like you to post the #thought_of_the_day. As was mentioned in the email, it can be any "science communication" that you find interesting. This can include latest science news, important scientific concepts, some scientific proof, and more than that. We would like to invite you to post your #thought_of_the_day on %s.
+
+Best,
+%s.""" % (receiver_name, receiver_day, admin)
+							# TODO: Send email here
+							server.sendmail(sender_email, receiver_email, message)
 except Exception as e:
 	# Print any error messages to stdout
 	print(e)
+
 finally:
 	server.quit() 
